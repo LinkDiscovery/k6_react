c4acbcb06fc653b95a4e4d18d8c30f45e96146e4
LinkDiscovery <shin920111@gmail.com>
2024-03-12 10:06:04 +0900

    0312

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import logo from './logo.svg'; //svg 는 이미지다 . 이미지도 import 형태로 가지고 온다. react는 
 import './App.css';
 // 컴포넌트 만들때 컴포넌트 명이 중요하다 , 컴포넌트 명은 대문자로 시작 
+
+// 화살표 함수로 작성가능 
+//const App = () => {}
+// return()
+
 function App() {
   return (
     //return 값은 한개만 되어야한다. 그래서 div가 2개로 나눠지면 오류가뜸 
